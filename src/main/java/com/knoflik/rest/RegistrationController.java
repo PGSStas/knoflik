fbbccda8c2898e285e6b9880c24cd8d0822e7c63
Stanislav <stas.katsuba@gmail.com>
2020-12-16 00:32:36 +0300

    fix

--- src/main/java/com/knoflik/rest/RegistrationController.java
+++ src/main/java/com/knoflik/rest/RegistrationController.java
@@ -35,16 +35,16 @@ public class RegistrationController {
                           final BindingResult bindingResult,
                           final Model model) {
         if (bindingResult.hasErrors()) {
-            return "registration";
+            return "redirect:/registration";
         }
         if (!userForm.getPassword().equals(userForm.getPasswordConfirm())) {
-            model.addAttribute("passwordError", "Пароли не совпадают");
-            return "registration";
+            model.addAttribute("passwordError", "true");
+            return "redirect:/registration";
         }
         if (!userService.saveUser(userForm)) {
             model.addAttribute("usernameError",
-                    "Пользователь с таким именем уже существует");
-            return "registration";
+                    "true");
+            return "redirect:/registration";
         }
 
         return "redirect:/";
